f7b58ac61f5160e86c9c8a796e62eb8b35b17d46
Kari-Prado <vitoria28prado@gmail.com>
2025-08-17 16:57:41 -0300

    Create TopicoRepository.java

---
+++ src/main/java/com/vitoriaPrado/forumhub/repository/TopicoRepository.java
@@ -0,0 +1,7 @@
+package com.vitoriaPrado.forumhub.repository;
+
+import com.vitoriaPrado.forumhub.model.Topico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+}
